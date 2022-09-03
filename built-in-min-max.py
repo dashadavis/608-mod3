141a301e63c4decbc0090a38c75ce1a84497e77e
Dasha Davis <dashadavis2013@gmail.com>
2022-09-03 06:59:23 -0500

    added code

---
+++ built-in-min-max.py
@@ -0,0 +1,9 @@
+print(max(12, 27, 36))
+
+print(min(15, 9, 27, 36))
+
+print("Hallelujah, Malcolm Phillips, for answering my discussion question! Thank you, Thank you")
+
+print("Sincerely, Dasha Davis")
+
+#Now I know that ipython and Jupyter automatically print (display results) for you. Running a script requires the coder to print results.
---
+++ calculator.py
@@ -0,0 +1,7 @@
+##Math Module Functions
+#Dasha Davis
+
+import math
+
+print(math.sqrt(900))
+print(math.floor(9.2))
